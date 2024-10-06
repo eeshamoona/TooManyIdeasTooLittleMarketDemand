d8900ba25685cef477c0b0b93a6afbebfe2ef5d5
Eesha Moona <eeshamoona@gmail.com>
2024-10-06 09:44:32 -0500

    re-deployment

--- monorepo/apps/project-one/thinking-traps/src/app/page.tsx
+++ monorepo/apps/project-one/thinking-traps/src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Heading } from "@chakra-ui/react";
-import { Button } from "ui/src/Button";
 export default function HomePage() {
   return (
     <div>
       <Heading>Welcome to My Next.js App!</Heading>
-      <Button colorScheme="blue">Click me!</Button>
     </div>
   );
 }
