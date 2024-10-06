9628701ad7ed11c6eebf2f61dbdd9b8e72b56b5c
Eesha Moona <eeshamoona@gmail.com>
2024-10-06 09:55:20 -0500

    re-deployment again

--- monorepo/apps/project-one/thinking-traps/next.config.mjs
+++ monorepo/apps/project-one/thinking-traps/next.config.mjs
@@ -1,4 +1,20 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: "out",  // Setting the output directory to 'out'
+  output: "export",  // Configuring for static export
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output.publicPath = path.join(__dirname, "out");
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
